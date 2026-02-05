50051111ca6250838da507c0713e4ce3f1b93551
Daneshwari <Daneshwari.Naganur1@ibm.com>
2026-02-05 18:27:59 +0530

    Add explicit hostname filter to exclude Kubernetes hosts

--- instana.tf
+++ instana.tf
@@ -346,18 +346,18 @@ resource "instana_alerting_channel" "robot_shop_email" {
 }
 
 # Alert Configuration - Link alerts to channels
-# Filters events only for Robot Shop application at APPLICATION LEVEL
+# Filters events ONLY for Robot Shop application on ubuntu hostname
 resource "instana_alerting_config" "robot_shop" {
   count = var.enable_instana_config ? 1 : 0
 
-  alert_name = "Robot Shop Application Health Alerts"
+  alert_name = "Robot Shop Application Health Alerts - ubuntu only"
   
   integration_ids = var.enable_instana_config ? [
     instana_alerting_channel.robot_shop_email[0].id
   ] : []
 
-  # Filter for Robot Shop application events only - APPLICATION LEVEL
-  event_filter_query = "entity.application.name:\"Robot-Shop-Microservices-Daneshwari-2026\" AND entity.type:application"
+  # Filter for Robot Shop application on ubuntu hostname ONLY - excludes Kubernetes hosts
+  event_filter_query = "entity.application.name:\"Robot-Shop-Microservices-Daneshwari-2026\" AND entity.type:application AND entity.host.name:\"ubuntu\" AND entity.zone:\"robot-shop-zone\""
   
   # Filter for application-level event types
   event_filter_event_types = [
